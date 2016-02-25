be3da6b401c727ca5332d04dc96f0c0a912090a5
whitingdj <djwhiting12@gmail.com>
2016-02-24 20:39:24 -0500

    added progress bar/ js for progress

--- js/main.js
+++ js/main.js
@@ -3,7 +3,14 @@ var $playButton = $(".play-btn");
 var $pauseButton = $(".pause-btn");
 var $muteButton = $(".mute-btn");
 var $volumeButton = $(".volume-btn");
-var $fullScreen = $(".full-screen");
+var $fullScreen = $("#full-screen");
+var $progress = $('#progress')[0];
+var $progressBar = $('#progress-bar')[0];
+
+
+/******************************************
+Play and Pause Buttons
+******************************************/
  
 $playButton.click(function () { 
 	$video.get(0).play(); //plays the video
@@ -17,6 +24,11 @@ $pauseButton.click(function () {
 	$('.play-btn').show();
 });
 
+
+/******************************************
+Volume and Mute Buttons
+******************************************/
+
 $muteButton.click(function () { 
 	$video.prop('muted', true); //mutes the video
 	$('.mute-btn').hide();
@@ -29,14 +41,63 @@ $volumeButton.click(function () {
 	$('.mute-btn').show();
 });
 
+/******************************************
+Full Screen Button
+******************************************/
+
 $fullScreen.click(function() {
-  if ($video.requestFullscreen) {
-    $video.requestFullscreen();
-  } else if ($video.msRequestFullScreen) {
-  	 $video.msRequestFullscreen();
-  } else if ($video.mozRequestFullScreen) {
-     $video.mozRequestFullScreen(); // Firefox
-  } else if ($video.webkitRequestFullscreen) {
-     $video.webkitRequestFullscreen(); // Chrome and Safari
+	var video = $video[0]; // redefine global var in order to manipulate true DOM methods
+  if (video.requestFullscreen) { //enter fullscreen
+    video.requestFullscreen();
+  } else if (video.msRequestFullScreen) {
+  	 video.msRequestFullscreen();
+  } else if (video.mozRequestFullScreen) {
+     video.mozRequestFullScreen(); // Firefox
+  } else if (video.webkitRequestFullscreen) {
+     video.webkitRequestFullscreen(); // Chrome and Safari
   }
 });
+
+$fullScreen.click(function() {
+	var video = $video[0]; // redefine global var in order to manipulate true DOM methods
+  if (video.exitFullscreen) { //exit fullscreen
+    video.exitFullscreen();
+  } else if (video.msExitFullscreen) {
+  	 video.msExitFullscreen();
+  } else if (video.mozCancelFullScreen) {
+     video.mozCancelFullScreen(); // Firefox
+  } else if (video.webkitExitFullscreen) {
+     video.webkitExitFullscreen(); // Chrome and Safari
+  }
+});
+
+
+/******************************************
+Progress Bar - JavaScript
+******************************************/
+
+$video[0].addEventListener('loadedmetadata', function() {
+   $progress.setAttribute('max', $video[0].duration);
+});
+$video[0].addEventListener('timeupdate', function() {
+   $progress.value = $video[0].currentTime;
+   $progressBar.style.width = Math.floor(($video[0].currentTime / $video[0].duration) * 100) + '%';
+});
+$video[0].addEventListener('timeupdate', function() {
+   if (!$progress.getAttribute('max')) $progress.setAttribute('max', $video[0].duration);
+   $progress.value = $video[0].currentTime;
+   $progressBar.style.width = Math.floor(($video[0].currentTime / $video[0].duration) * 100) + '%';
+});
+
+
+/******************************************
+Skip Ahead- JavaScript
+******************************************/
+
+$progress.click(function(e) {
+   var pos = (e.pageX  - this.offsetLeft) / this.offsetWidth;
+   $video[0].currentTime = pos * video[0].duration;
+});
+
+/*$('#current').append($video[0].currentTime);
+$('#duration').append($video[0].duration);*/
